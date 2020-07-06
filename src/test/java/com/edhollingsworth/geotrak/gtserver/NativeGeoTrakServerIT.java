9e0985f2645afc5eab126dfd07cd1609ce8dde75
Ed Hollingsworth <ed.hollingsworth@ionep.com>
2020-07-06 05:32:38 -0400

    Freshly-created Quarkus app

---
+++ src/test/java/com/edhollingsworth/geotrak/gtserver/NativeGeoTrakServerIT.java
@@ -0,0 +1,9 @@
+package com.edhollingsworth.geotrak.gtserver;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGeoTrakServerIT extends GeoTrakServerTest {
+
+    // Execute the same tests but in native mode.
+}
